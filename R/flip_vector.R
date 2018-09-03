855bbf81eef438e849842bd39d03e8d736ae5738
pedrostrusso <pedrostrusso@gmail.com>
2018-09-03 16:09:19 -0300

    Adding flip_vector documentation

--- R/flip_vector.R
+++ R/flip_vector.R
@@ -1,5 +1,18 @@
+#' @importFrom stats setNames
+NULL
+
+#' Flips a vector's values and its names
+#'
+#' @param x Vector to be flipped
+#' 
+#' @details This function is simply a wrapper for \code{setNames(names(x), x)}
+#'
+#' @export
+
+#' @examples
+#' x <- c(1, 2, 3)
+#' names(x) <- c("a", "b", "c")
+#' flip_vector(x)
 flip_vector <- function(x){
-    res <- names(x)
-    names(res) <- x
-    return(res)
+    return(setNames(names(x), x))
 }
