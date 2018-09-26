3dfe3c17dcc89315e6b31ece36c6d664178062c3
pedrostrusso <pedrostrusso@gmail.com>
2018-09-26 14:54:19 -0300

    Updating multi_lapply

--- R/multi_lapply.R
+++ R/multi_lapply.R
@@ -1,3 +1,16 @@
+#' Run multiple functions on a list
+#'
+#' @param ... Arguments containing the list to be transformed, and functions to apply. The first
+#' argument should be the list, and following arguments should be the functions.
+#'
+#' @details This function is used as an alternative to piping consecutive lapplys
+#'
+#' @return A list
+#' @export
+#'
+#' @examples
+#' split_cars <- split(mtcars, rep(1:3, length.out=nrow(mtcars), each=ceiling(nrow(mtcars)/3)))
+#' res <- multi_lapply(split_cars, t, as.data.frame)
 multi_lapply <- function(...) {
     arglist <- match.call(expand.dots = FALSE)$...
     var.names <- sapply(arglist, deparse)
@@ -10,5 +23,5 @@ multi_lapply <- function(...) {
     for(func in arglist){
         lst <- lapply(lst, func)
     }
-    return(x2)
+    return(lst)
 }
